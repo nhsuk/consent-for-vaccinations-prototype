ce8ad48e24c79cfcffb483c17b0aba6b82f8352f
Paul Hayes <fofr@users.noreply.github.com>
2022-12-13 14:47:14 +0000

    Iterations to the flu journey from research

--- app/wizards/flu.js
+++ app/wizards/flu.js
@@ -1,32 +1,46 @@
 import { wizard } from 'nhsuk-prototype-rig'
 
 export function fluWizard (req) {
-  const consented = req.session.data.consent !== 'No'
+  const consentedNasal = req.session.data.consent !== 'No'
+  const consentedJab = req.session.data['im-consent'] !== 'No'
 
   const journey = {
     '/flu/start': {},
     '/flu/consent/email': {},
     '/flu/consent/confirm-email': {},
-    '/flu/consent/parent-guardian': {},
     '/flu/consent/child': {},
     '/flu/consent/child-dob': {},
     '/flu/consent/school': {},
+    '/flu/consent/parent-guardian': {},
     '/flu/consent/consent': {
-      '/flu/consent/child-gp': consented
+      '/flu/consent/child-gp': consentedNasal
     },
     '/flu/consent/no-consent-given': {
+      '/flu/im-consent': true,
       '/flu/consent/check-answers': true
     },
+    '/flu/im-consent': {
+      '/flu/consent/check-answers': !consentedJab
+    },
     '/flu/consent/child-gp': {},
-    '/flu/consent/child-nhs': {},
+    // '/flu/consent/child-nhs': {},
     '/flu/consent/health-questions': {},
     '/flu/consent/health-immune-system': {},
-    '/flu/consent/health-household-immune-system': {},
-    '/flu/consent/health-asthma': {},
+    ...consentedNasal ? {
+      '/flu/consent/health-household-immune-system': {},
+      '/flu/consent/health-asthma': {
+        '/flu/consent/health-egg-allergy': {
+          data: 'health.asthma',
+          value: 'No'
+        }
+      },
+      '/flu/consent/health-asthma-steroids': {},
+      '/flu/consent/health-aspirin': {}
+    } : {},
     '/flu/consent/health-egg-allergy': {},
-    '/flu/consent/health-aspirin': {},
+    '/flu/consent/health-anything-else': {},
     '/flu/consent/check-answers': {
-      '/flu/consent/confirmation-no-consent': !consented
+      '/flu/consent/confirmation-no-consent': !consentedNasal && !consentedJab
     },
     '/flu/consent/confirmation': {},
     '/': {}
