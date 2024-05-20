8749d10afea65863f4e92e702200eb218829586a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-05-20 12:47:29 +0200

    Reinstate common locals across all paths

--- app/routes.js
+++ app/routes.js
@@ -7,11 +7,21 @@ import { saisRoutes } from './routes/sais.js'
 
 const router = express.Router()
 
-router.all('/emails/flu/*', (req, res, next) => {
-  res.locals.childName = 'Jamie Doe'
-  res.locals.knownAs = ' (known as Jay)'
+router.all('*', (req, res, next) => {
+  if (req.session.data?.child?.['first-name']) {
+    res.locals.childName = `${req.session.data.child['first-name']} ${req.session.data.child['last-name']}`
+  } else {
+    res.locals.childName = 'Jamie Doe'
+    res.locals.knownAs = ' (known as Jay)'
+  }
+
   res.locals.deadlineDate = '2023-11-24'
   res.locals.sessionDate = '2023-11-27'
+
+  next()
+})
+
+router.all('/emails/flu/*', (req, res, next) => {
   res.locals.primary = true
   res.locals.sessionSchool = 'St Mary’s Primary School'
 
@@ -19,10 +29,6 @@ router.all('/emails/flu/*', (req, res, next) => {
 })
 
 router.all('/emails/hpv/*', (req, res, next) => {
-  res.locals.childName = 'Matthew Doe'
-  res.locals.knownAs = ' (known as Matty)'
-  res.locals.deadlineDate = '2023-11-24'
-  res.locals.sessionDate = '2023-11-27'
   res.locals.primary = false
   res.locals.sessionSchool = 'Hele’s Secondary School'
 
